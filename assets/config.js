921e94f0bdd17ec46c5a7ec5fc118f4ba88cd9d0
diegomezapy <dmeza.py@gmail.com>
2026-06-06 16:25:23 -0400

    Publish sheet-associated Apps Script endpoint

--- assets/config.js
+++ assets/config.js
@@ -1,10 +1,10 @@
 window.LICITABAYES_CONFIG = {
   appName: "LicitaBayes DNCP",
-  appVersion: "0.1.11",
+  appVersion: "0.1.12",
   adminEmail: "dmeza.py@gmail.com",
   dataBaseUrl: "data",
   githubPagesUrl: "https://diegomezapy.github.io/tableroDNCPpy/",
   googleSheetId: "1QJ_xagB5ze4ugYIpOosYPHBsp-o7TfQM8W8FUYSUnmQ",
-  gasEndpoint: "https://script.google.com/macros/s/AKfycbykfPCyABmMx63lXkaBT4v3kUvKltQ33m8Pfc5eh93K7Ev5shyQpJL2C4CRH9GOkVOI2g/exec",
+  gasEndpoint: "https://script.google.com/macros/s/AKfycbzweRdG2sBKnIDSnj7GFaBRT3YAeKgOylce8CXuHZ5bH-tNltwHpweT1AB1K7VGKhmb/exec",
   maxTableRows: 250
 };
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "licitabayes-dncp-v0-1-11";
+const CACHE_NAME = "licitabayes-dncp-v0-1-12";
 const APP_SHELL = [
   "./",
   "index.html",
