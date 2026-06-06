95bd73160a8156170b5b519d1a09f4d99a22882d
diegomezapy <dmeza.py@gmail.com>
2026-06-06 15:30:40 -0400

    Publish mini meter overlap fix

--- assets/config.js
+++ assets/config.js
@@ -1,10 +1,10 @@
 window.LICITABAYES_CONFIG = {
   appName: "LicitaBayes DNCP",
-  appVersion: "0.1.8",
+  appVersion: "0.1.9",
   adminEmail: "dmeza.py@gmail.com",
   dataBaseUrl: "data",
   githubPagesUrl: "https://diegomezapy.github.io/tableroDNCPpy/",
   googleSheetId: "1QJ_xagB5ze4ugYIpOosYPHBsp-o7TfQM8W8FUYSUnmQ",
-  gasEndpoint: "https://script.google.com/macros/s/AKfycbwVWTZi6vCVBJMDpGbJb4DShxxH2GrsB0MFONtRQgal7AMrZscaiXC_IpTRm9m9QI648A/exec",
+  gasEndpoint: "https://script.google.com/macros/s/AKfycbz1TsUrLQRwCea1irQhqdNPMqsc4zM7Sbbc23-RQuldXNVWPuNHQymM8YwFafWS2FyBdw/exec",
   maxTableRows: 250
 };
