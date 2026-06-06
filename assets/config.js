272cdb9bce97cf4beb6a8cdb961042c34999d645
diegomezapy <dmeza.py@gmail.com>
2026-06-06 12:01:46 -0400

    Publish improved price reference model

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.LICITABAYES_CONFIG = {
   appName: "LicitaBayes DNCP",
-  appVersion: "0.1.1",
+  appVersion: "0.1.2",
   dataBaseUrl: "data",
   githubPagesUrl: "https://diegomezapy.github.io/tableroDNCPpy/",
   googleSheetId: "1QJ_xagB5ze4ugYIpOosYPHBsp-o7TfQM8W8FUYSUnmQ",
