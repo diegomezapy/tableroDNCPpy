9e6f82078da47ae32f491206476fded3d6a0e2e0
diegomezapy <dmeza.py@gmail.com>
2026-06-06 15:42:31 -0400

    Publish citizen value Bayes methodology

--- assets/config.js
+++ assets/config.js
@@ -1,10 +1,10 @@
 window.LICITABAYES_CONFIG = {
   appName: "LicitaBayes DNCP",
-  appVersion: "0.1.9",
+  appVersion: "0.1.10",
   adminEmail: "dmeza.py@gmail.com",
   dataBaseUrl: "data",
   githubPagesUrl: "https://diegomezapy.github.io/tableroDNCPpy/",
   googleSheetId: "1QJ_xagB5ze4ugYIpOosYPHBsp-o7TfQM8W8FUYSUnmQ",
-  gasEndpoint: "https://script.google.com/macros/s/AKfycbz1TsUrLQRwCea1irQhqdNPMqsc4zM7Sbbc23-RQuldXNVWPuNHQymM8YwFafWS2FyBdw/exec",
+  gasEndpoint: "https://script.google.com/macros/s/AKfycbw57cvchN8cQsUoa8pyJfn3fEv4avtxC-F8-3DtmGWZ24Iv_A634aVgdzA0xGEkmRrL-w/exec",
   maxTableRows: 250
 };
